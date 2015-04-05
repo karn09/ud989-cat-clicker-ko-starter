4983379a7a6d817da6e7e1f1eabe8c29cc8d97c4
karn09 <karn09@gmail.com>
2015-04-05 03:50:59 +0000

    added list of cats, and click event to change current cat displayed

--- js/app.js
+++ js/app.js
@@ -1,14 +1,43 @@
+var initialCats = [
+  { 
+    clickCount: 0,
+    name: 'Tabby',
+    imgSrc: 'img/1413379559_412a540d29_z.jpg',
+    nicknames: ['tabtab', 'boogiie', 'katto']
+  }, {
+    clickCount: 0,
+    name: 'Tiger',
+    imgSrc: 'img/22252709_010df3379e_z.jpg',
+    nicknames: ['Tigger']
+  }, {
+    clickCount: 0,
+    name: 'Scaredy',
+    imgSrc: 'img/4154543904_6e2428c421_z.jpg',
+    nicknames: ['Scaredoooo']
+  }
+    
+]
 var ViewModel = function() {
-  this.clickCount = ko.observable(0);
-  this.name = ko.observable('Tabby');
-  this.imgSrc = ko.observable('img/1413379559_412a540d29_z.jpg');
-  this.levels = ko.observable(['kitten', 'teen kitty', 'cat'])
   
-
+  var self = this;
+  this.catList = ko.observableArray([]);
+ 
+  initialCats.forEach(function(catItem) {
+    self.catList.push(new Cat(catItem))
+  })
+  this.changeCat = function(cat) {
+    console.log(cat)
+    self.currentCat(cat);
+  }
+  this.currentCat = ko.observable(this.catList()[0]);
   this.incrementCounter = function() {
     return this.clickCount(this.clickCount() + 1);
   }
+}
 
+var Cat = function(data) {
+  this.name = ko.observable(data.name);
+  
   this.level = ko.pureComputed(function() {
     if (this.clickCount() < 10) {
       return 'Teeeensy Kitty!'
@@ -19,7 +48,9 @@ var ViewModel = function() {
     }
   }, this);
   
-  this.nicknames = ko.observable(['a', 'b', 'c']);
+  this.clickCount = ko.observable(data.clickCount);
+  this.imgSrc = ko.observable(data.imgSrc); 
+  this.nicknames = ko.observable(data.nicknames);
   
 }
 
